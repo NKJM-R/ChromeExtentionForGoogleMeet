fbfb9019c0f5d2a006ca2d01d5ed797a9e41db42
git-eg74-nakajima <nakajima.ryota.eg74@members.co.jp>
2020-07-22 20:18:31 +0900

    雑だけ実装

--- content_script.js
+++ content_script.js
@@ -1,73 +1,133 @@
+const SOUND_CONFIG = "./sound.json";
+const MEET_CONFIG = "./meet.json";
+const LOGOUT = "LOGOUT";
+const TAIKO = "https://soundeffect-lab.info/sound/anime/mp3/drum-japanese2.mp3";
+
 class Sound {
   constructor(source) {
     // 音源
     this.source = source;
   }
   play() {
-    Audio(this.source).play();
-  }
-}
-
-class Person {
-  constructor() {
-    this.name = null;
-    this.image = null;
+    new Audio(this.source).play();
   }
 }
 
 class Meet {
   constructor() {
-    this.sounds = {};
-    this.people = [];
-    this.events = ["LOGOUT"];
-  }
+    this.USER_COUNT_ELEMENT = "wnPUne N0PJ8e";
 
-  addMember(member) {
-    this.members.push(member);
+    this.sounds = {};
+    this.observers = {};
+    this.userCount = 0;
   }
 
   setSound(name, source) {
-    this.sound[name] = new Sound(source);
+    this.sounds[name] = new Sound(source);
   }
 
   playSound(name) {
     this.sounds[name].play();
   }
 
-  logout(soundName) {
-    this.playSound(soundName);
+  syncUserCount(stop = false) {
+    stop
+      ? this._syncStop(this.USER_COUNT_ELEMENT)
+      : this._syncStart(this.USER_COUNT_ELEMENT, this._setUserCount);
   }
+
+  _setUserCount = (element) => (this.userCount = element[0].target.innerHTML);
+  _syncStop = (name) => this.observers[name].disconnect();
+
+  _syncStart(className, fc) {
+    const element = document.getElementsByClassName(className)[0];
+    this.observers[className] = new MutationObserver((records) => {
+      fc(records);
+    });
+    this.observers[className].observe(element, {
+      childList: true,
+      characterData: true,
+    });
+  }
+
+  _executeElementPropaty(element, propaty) {}
 }
 
-class EventObserve {
+class EventObserver {
   constructor(meet) {
     this.meet = meet;
   }
+
   run() {
-    // @TODO: 考慮の余地あり
-    // this.enventWatch();
+    const userCount = "userCount";
+    this.propsWatch(userCount);
   }
 
-  eventPerception() {
-    if (false) this.eventLogin();
-    if (false) this.eventLogout();
+  eventSwitcher(newValue, propaty) {
+    console.log(this.meet);
+    console.log(propaty);
+    const oldValue = this.meet[propaty];
+    if (oldValue > newValue) this.meet.playSound(LOGOUT);
   }
 
-  eventLogout() {}
-
-  eventLogin() {}
-
-  // @TODO: 考慮の余地あり
-  eventWatch(propaty) {
+  propsWatch(propaty) {
     let value = this.meet[propaty];
     Object.defineProperty(this.meet, propaty, {
       get: () => value,
-      set: () => {},
+      set: (newValue) => {
+        this.eventSwitcher(newValue, propaty);
+      },
       configurable: true,
     });
   }
 }
 
+class GoogleMeetExtention {
+  constructor(meet, observer) {
+    this.isRunning = false;
+    this.meet = meet;
+    this.observer = observer;
+    this.behavior = {
+      UP: () => this.up(),
+      DOWN: () => this.down(),
+    };
+  }
+
+  up() {
+    if (this.isStarted) return "Already running";
+    this.meet.syncUserCount();
+    this.observer.run();
+    this.isRunning = true;
+    return "Running";
+  }
+
+  down() {
+    if (!this.isStarted) return "Not running";
+    const stop = true;
+    this.meet.syncUserCount(stop);
+    this.observer.run();
+    this.isRunning = false;
+    msg = "Stopped";
+    return msg;
+  }
+
+  waitForOrder() {
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) =>
+      this._listener(request, sender, sendResponse)
+    );
+  }
+
+  _listener(request, sender, sendResponse) {
+    const order = request.message;
+    const msg = this.behavior[order]();
+    sendResponse(msg);
+  }
+}
+
 const meet = new Meet();
-const observer = new EventObserve(meet);
-observer.run();
+meet.setSound(LOGOUT, TAIKO);
+
+const observer = new EventObserver(meet);
+
+gme = new GoogleMeetExtention(meet, observer);
+gme.waitForOrder();
--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
-document.getElementById('black').onclick = function () {
-    async () => {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { text: "msg" }, function (response) {
-                console.log(response);
-            });
-        });
-    }
-}
+document.getElementById("button").onclick = () => {
+  console.log("起動リクエスト");
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { message: "UP" }, response);
+  });
+};
+
+response = (item) => {
+  console.log(item);
+};
